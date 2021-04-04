c46d5beac560a82b87e6163ac4039fe2f965b4f2
geraldpd <dasallagerald@gmail.com>
2021-04-04 21:41:55 +0800

    first push

---
+++ config/eems.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'roles' => [
+        'admin',
+        'organizer',
+        'attendee',
+    ]
+
+];
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	foreach(config('eems.roles') as $role) {
+	        $user = User::create([
+				'firstname' => $role,
+                'lastname' => $role,
+                'mobile_number' => '900000000'.rand(0,9),
+				'email' => $role.'@'.config('app.name').'.com',
+				'email_verified_at' => Carbon::now(),
+				'password' => bcrypt('password'),
+                'remember_token' => Str::random(10),
+	        ]);
+	        $user->assignRole($role);
+		}
+    }
+}
