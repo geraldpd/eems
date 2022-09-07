701f6dfe6f3306f032994b6a6a916eab6842c107
Gerald Dasalla <gerald@Geralds-MacBook-Air.local>
2022-09-07 10:20:36 +0800

    improved event live status preview

--- public/scripts/organizer/events/index.js
+++ public/scripts/organizer/events/index.js
@@ -31,20 +31,6 @@ $(function() {
             $('.add-event-button')
             .attr('href', config.routes.create + `/?start=${start}&end=${end}`)
 
-            /*
-            let eventsOfTheDay = Object.keys(config.events).map(function(key) {
-                let keyRange = key.split(',')
-                if (
-                    info.dateStr > keyRange[0] && info.dateStr < keyRange[1] ||
-                    (info.dateStr == keyRange[0])
-                    ) {
-                    return config.events[key]
-                }
-            })
-            .filter(Boolean)
-            .flat();
-            */
-
             fetchScheduleEvents({
                 start: start,
                 end: end
@@ -71,42 +57,51 @@ $(function() {
 
             $('.add-event-button')
             .attr('href', config.routes.create + `/?start=${start}&end=${end}`)
-            .show()
 
             fetchScheduleEvents({
                 start: start,
                 end: end
             }, eventsOfTheDay => {
 
+                let events = ''
+
                 if (Object.keys(eventsOfTheDay).length) {
-
-                    let events = $.map(eventsOfTheDay, function(events, index) {
-                       return `<h3>${moment(index).format('MMMM D YYYY')}</h3> <div class="col-md-12"> ${constructEventList(events, index)}</div><hr>`
+                    events = $.map(eventsOfTheDay, function(events, index) {
+                        return `<h3>${moment(index).format('MMMM D YYYY')}</h3> <div class="col-md-12"> ${constructEventList(events, index)}</div><hr>`
                     })
-
-                    dateModal(info, events, moment(info.dateStr).isAfter()); //moment(info.dateStr).isAfter() compares the moment object if its after NOW date
-
                 } else {
-
-                   let events =  '<h3>No event scheduled for this date!</h3>'
-                   dateModal(info, events, moment(info.dateStr).isAfter()); //moment(info.dateStr).isAfter() compares the moment object if its after NOW date
+                    events =  '<h3>No event scheduled for this date!</h3>'
                 }
+
+                dateModal(info, events, moment(info.startStr).isAfter()); //moment(info.dateStr).isAfter() compares the moment object if its after NOW date
             })
         }
     });
 
     function dateModal(info, events, add_buton_bool = true) {
+        let eventCount = 0;
         let readableFormat = date => moment(date).format('MMMM Do YYYY');
         let event_for = info.dateStr
-                    ? readableFormat(info.dateStr)
-                    : `${readableFormat(info.startStr)} - ${readableFormat(moment(info.endStr).subtract(1, 'days'))}`;
+        ? readableFormat(info.dateStr)
+        : `${readableFormat(info.startStr)} - ${readableFormat(moment(info.endStr).subtract(1, 'days'))}`;
 
         modals.date.modal('show');
         modals.date.find('.date-title').text(`Events for ${ event_for }`);
         modals.date.find('.date-events').html(events);
 
+        modals.date.find('.toggle-concluded-events').off().on('click', _ => {
+            modals.date.find('.event').each((i, event_div) => {
+
+                let concludedEvent = $(event_div).data('status') == 'CONCLUDED'
+
+                if(concludedEvent) {
+                    $(event_div).toggle(!$(event_div).is(':visible'))
+                }
+            })
+        })
 
         modals.date.find('.event-status').each((i, status_div) => {
+            eventCount += 1;
             let status_data = $(status_div).data();
 
             let schedule_start = moment(status_data.start)
@@ -118,35 +113,53 @@ $(function() {
                 let status_html = ''
                 switch (status_data.status) {
                     case 'CONCLUDED':
-                        status_html = '<b class="text-muted">CONCLUDED</b >'
-                        break;
+                    status_html = '<b class="text-muted">CONCLUDED</b >'
+                    break;
 
                     case 'ONGOING':
+                    let timeDiffEnd = moment.duration(moment(schedule_end).diff(moment())); //! for ongoing to concluded
+                    let secondsLeftEnd = parseFloat(timeDiffEnd.format("s").replace(/,/g, ''))
+
+                    if(secondsLeftEnd < 0) {
+                        status_html = '<b class="text-muted">CONCLUDED</b >'
+                        clearInterval(intervals[i])
+                    } else {
                         status_html = '<b class="text-success">ONGOING</b >'
-                        break;
+                    }
+                    break;
 
                     case 'PENDING':
-                        status_html = '<b class="text-muted">PENDING</b >'
-                        break;
+                    status_html = '<b class="text-muted">PENDING</b >'
+                    break;
 
                     default: //*SOON
-                        let timeDiff = moment.duration(moment(schedule_start).diff(moment()));
-                        let countdown = timeDiff.format("hh : mm : ss")
-                        let secondsLeft = parseFloat(timeDiff.format("s").replace(/,/g, ''))
+                    let timeDiff = moment.duration(moment(schedule_start).diff(moment()));
+                    let countdown = timeDiff.format("hh : mm : ss")
+                    let secondsLeft = parseFloat(timeDiff.format("s").replace(/,/g, ''))
 
-                        if(secondsLeft > 0) {
-                            status_html = `<small>starts in</small> <h4 title="Event Countdown" ><b class="event-status-timer-disabled text-warning">${countdown}</b></h4>`;
-                        } else {
-                            //let timeDiff = moment.duration(moment(schedule_end).diff(moment())); //! for ongoing to concluded
+                    if(secondsLeft > 0) {
+                        status_html = `<small>starts in</small> <h4 title="Event Countdown" ><b class="event-status-timer-disabled text-warning">${countdown}</b></h4>`;
+                    } else {
+                        let timeDiffEnd = moment.duration(moment(schedule_end).diff(moment())); //! for ongoing to concluded
+                        let secondsLeftEnd = parseFloat(timeDiffEnd.format("s").replace(/,/g, ''))
+
+                        if(secondsLeftEnd > 0) {
                             status_html = '<b class="text-success">ONGOING</b >'
-                            clearInterval(i);
+                        } else {
+                            status_html = '<b class="text-muted">CONCLUDED</b >'
+                            clearInterval(intervals[i])
                         }
-                        break;
+                    }
+                    break;
                 }
 
                 $(status_div).html(status_html);
             }, 1000);
         });
+
+        eventCount ? modals.date.find('.toggle-concluded-events').show() : modals.date.find('.toggle-concluded-events').hide()
+
+        $('.add-event-button').toggle(add_buton_bool)
     }
 
     function constructEventList(schedules, day) {
@@ -154,60 +167,45 @@ $(function() {
             let date_formater = (date) => ({time: moment(date).format('hh:mm A'), day: moment(date).format('MMMM D YYYY')});
 
             /*
-                check if the the event is scheduled today or was in the past
-                *TRUE - do not render the edit button
-                !FALSE - render the edit button redirecting to the edit page
+            check if the the event is scheduled today or was in the past
+            *TRUE - do not render the edit button
+            !FALSE - render the edit button redirecting to the edit page
             */
             let edit_button = moment(schedule.schedule_start).isBefore() ? '' : `<a class=" btn btn-link" href="${config.routes.edit.replace('resource_id', schedule.event.code)}">Edit</a>`;
 
             return `
-                <div class="event row">
-                    <div class="col-md-7 col-sm-12">
-                        <br>
-                        <h2 class="lead">${schedule.event.name}</h2>
-                        <p>
-                            Location: <b>${schedule.event.location}</b>
-                            <br>
-                            Category: <b>${schedule.event.category.name}</b>
-                            <br>
-                            Type: <b>${schedule.event.type.name}</b>
-                        </p>
-                    </div>
-                    <div class="col-md-5 col-sm-12">
-                        <br>
+            <div class="event row" data-status="${schedule.status}">
+            <div class="col-md-7 col-sm-12">
+            <br>
+            <h2 class="lead">${schedule.event.name}</h2>
+            <p>
+            Location: <b>${schedule.event.location}</b>
+            <br>
+            Category: <b>${schedule.event.category.name}</b>
+            <br>
+            Type: <b>${schedule.event.type.name}</b>
+            </p>
+            </div>
+            <div class="col-md-5 col-sm-12">
+            <br>
 
-                        <h4 class="scheduled_time">
-                            <strong>${date_formater(schedule.schedule_start).time}</strong> - <strong>${date_formater(schedule.schedule_end).time}</strong>
-                        </h4>
+            <h4 class="scheduled_time">
+            <strong>${date_formater(schedule.schedule_start).time}</strong> - <strong>${date_formater(schedule.schedule_end).time}</strong>
+            </h4>
 
-                        <div class="event-status" data-status="${schedule.status}" data-day="${day}" data-start="${ schedule.schedule_start }" data-end="${ schedule.schedule_end }"></div>
+            <div class="event-status" data-status="${schedule.status}" data-day="${day}" data-start="${ schedule.schedule_start }" data-end="${ schedule.schedule_end }"></div>
 
-                        <a class=" btn btn-link btn-light" href="${config.routes.show.replace('resource_id', schedule.event.code)}">Preview</a>
+            <a class=" btn btn-link btn-light" href="${config.routes.show.replace('resource_id', schedule.event.code)}">Preview</a>
 
-                        <a class=" btn btn-link btn-light" href="${config.routes.invitations.replace('resource_id', schedule.event.code)}">Attendees</a>
+            <a class=" btn btn-link btn-light" href="${config.routes.invitations.replace('resource_id', schedule.event.code)}">Attendees</a>
 
-                        ${edit_button}
-                    </div>
-                </div>`;
+            ${edit_button}
+            </div>
+            </div>`;
 
         };
 
-        return schedules.map(schedule => event_row(schedule)).join('<br>');
-    }
-
-    function constructListOfIntervals(start, end, interval) {
-        const intervals = {};
-
-        while (moment(end).diff(start, interval) > 0) {
-          const current_end = moment(moment(start).add(1, interval)).format(moment_format);
-
-          Object.assign(intervals, { [start]: current_end });
-
-          start = current_end;
-        }
-
-        //return intervals;
-        return Object.keys(intervals);
+        return schedules.map(schedule => event_row(schedule)).join('');
     }
 
     function fetchScheduleEvents(date, callback) {
@@ -216,7 +214,7 @@ $(function() {
             callback(response.data)
         })
         .catch(function (error) {
-          console.warn(error);
+            console.warn(error);
         });
     }
 
--- resources/views/organizer/events/index.blade.php
+++ resources/views/organizer/events/index.blade.php
@@ -1,97 +1,106 @@
 @extends('layouts.organizer')
 
 @section('content')
-    @if(session()->has('message'))
-        <div class="alert alert-info">
-            {{ session()->get('message') }}
-        </div>
-    @endif
+@if(session()->has('message'))
+<div class="alert alert-info">
+    {{ session()->get('message') }}
+</div>
+@endif
 
-    <div id='calendar'></div>
+<div id='calendar'></div>
 @endsection
 
 @push('modals')
-    <div class="modal fade" id="date-modal" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title date-title"></h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="date-events">
-                        <div class="event row container">
-                            <div class="col-md-6">
-                                <span>Location</span>
-                                <h2>Name</h2>
-                                <p>
-                                    <span>start</span> - <span>end</span>
-                                </p>
-                            </div>
-                            <div class="col-md-6">
-                                <p>Category: <b>Category</b></p>
-                                <p>Type: <b>type</b></p>
-                            </div>
+<div class="modal fade" id="date-modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title date-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="date-events">
+                    <div class="event row container">
+                        <div class="col-md-6">
+                            <span>Location</span>
+                            <h2>Name</h2>
+                            <p>
+                                <span>start</span> - <span>end</span>
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>Category: <b>Category</b></p>
+                            <p>Type: <b>type</b></p>
                         </div>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <a href="#" class="btn btn-primary add-event-button">Add Event</a>
-                </div>
+            </div>
+            <div class="modal-footer modal-footer--sticky">
+                <button type="button" class="btn btn-link pull-left toggle-concluded-events" >Toggle concluded events</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <a href="#" class="btn btn-primary add-event-button">Add Event</a>
             </div>
         </div>
     </div>
+</div>
 @endpush
 
 @push('styles')
-    <style>
-        .event {
-            background-color: #e6e6e642;
-            margin-right: 5px;
-            margin-left: 5px;
-            border-radius: 12px;
-        }
+<style>
+    .event {
+        background-color: #e6e6e642;
+        margin-right: 5px;
+        margin-left: 5px;
+        margin-bottom: 10px;
+        border-radius: 12px;
+    }
 
-        .fc-daygrid-event:hover{
-            transform: scale(1.1);
-            cursor: pointer;
-        }
+    .fc-daygrid-event:hover{
+        transform: scale(1.1);
+        cursor: pointer;
+    }
 
-        .fc .fc-daygrid-day.fc-day-today {
-            background: white !important;
-            border: 3px solid #2c3e50;
-        }
+    .fc .fc-daygrid-day.fc-day-today {
+        background: white !important;
+        border: 3px solid #2c3e50;
+    }
 
-        @font-face {
-            font-family: 'digital7';
-            font-weight: normal;
-            font-style: normal;
-            src: url('{{asset('/fonts/custom/digital-7.ttf')}}') format('truetype');
-        }
+    @font-face {
+        font-family: 'digital7';
+        font-weight: normal;
+        font-style: normal;
+        src: url('{{asset('/fonts/custom/digital-7.ttf')}}') format('truetype');
+    }
 
-        .event-status-timer {
-            font-family: 'digital7', sans-serif;
-        }
+    .event-status-timer {
+        font-family: 'digital7', sans-serif;
+    }
 
-        .add-events-button {
-            display:none;
-        }
-    </style>
+    .add-events-button {
+        display:none;
+    }
+    /* Footer fixed to the bottom of the modal */
+    .modal-footer--sticky {
+        position: sticky;
+        bottom: 0;
+        background-color: inherit; /* [1] */
+        z-index: 1055; /* [2] */
+    }
+</style>
 @endpush
 
 @push('scripts')
-    <script type="text/javascript">
-        const config = {
-            routes: @json(routes('events', null, 'organizer')),
-            events: @json($events)
-        }
+<script type="text/javascript">
+    const config = {
+        routes: @json(routes('events', null, 'organizer')),
+        events: @json($events)
+    }
 
-        config.routes.fetchScheduleEvents = '{{ route('organizer.events.fetch-scheduled-events') }}'
-        config.routes.invitations = '{{ route('organizer.invitations.index', ['resource_id']) }}'
-        config.routes.create = '{{ route('organizer.events.create') }}'
-    </script>
-    <script src="{{ asset('scripts/organizer/events/index.js') }}"></script>
+    config.routes.fetchScheduleEvents = '{{ route('organizer.events.fetch-scheduled-events') }}'
+    config.routes.invitations = '{{ route('organizer.invitations.index', ['resource_id']) }}'
+    config.routes.create = '{{ route('organizer.events.create') }}'
+</script>
+<script src="{{ asset('scripts/organizer/events/index.js') }}"></script>
 @endpush
